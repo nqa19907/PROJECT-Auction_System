fbbf5155788a7048404234000b2c4c44d891c83f
Quoc Anh <nqa19907@gmail.com>
2026-05-01 20:16:08 +0700

    fix factory method files

--- AUCTION_SYSTEM/auction/src/main/java/auction_system/common/patterns/factory/ElectronicCreator.java
+++ AUCTION_SYSTEM/auction/src/main/java/auction_system/common/patterns/factory/ElectronicCreator.java
@@ -1,19 +1,24 @@
 package auction_system.common.patterns.factory;
+
 import auction_system.common.models.Electronic;
 import auction_system.common.models.Item;
+import java.util.Map;
 
+/**
+ * Lớp Factory chịu trách nhiệm khởi tạo các sản phẩm là thiết bị điện tử (Electronic).
+ */
 public class ElectronicCreator implements ItemCreator {
     @Override
-    public Item createItem() {
+    public Item createItem(Map<String, Object> properties) {
         return new Electronic(
-                "New Electronic Item",
-                "Description here",
-                0.0,
-                "SYSTEM",
-                "New",
-                "none",
-                "Generic Brand",
-                0
+                (String) properties.getOrDefault("itemName", "New Electronic Item"),
+                (String) properties.getOrDefault("description", "Description here"),
+                ((Number) properties.getOrDefault("startPrice", 0.0)).doubleValue(),
+                (String) properties.getOrDefault("sellerId", "SYSTEM"),
+                (String) properties.getOrDefault("condition", "New"),
+                (String) properties.getOrDefault("imagePath", "none"),
+                (String) properties.getOrDefault("brand", "Generic Brand"),
+                ((Number) properties.getOrDefault("warrantyMonths", 0)).intValue()
         );
     }
 }
