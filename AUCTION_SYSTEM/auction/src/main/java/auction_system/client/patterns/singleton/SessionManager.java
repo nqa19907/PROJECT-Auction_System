3659f9d5bf78c8779a12ba293e46e6139bb18614
Quoc Anh <nqa19907@gmail.com>
2026-05-02 14:05:22 +0700

    k

--- AUCTION_SYSTEM/auction/src/main/java/auction_system/client/patterns/singleton/SessionManager.java
+++ AUCTION_SYSTEM/auction/src/main/java/auction_system/client/patterns/singleton/SessionManager.java
@@ -1,4 +1,4 @@
-package auction_system.client.patterns.Singleton;
+package auction_system.client.patterns.singleton;
 
 /**
  * Lớp quản lý phiên làm việc của người dùng áp dụng mẫu thiết kế Singleton.
