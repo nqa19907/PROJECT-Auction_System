75fa2074d9efa113f460529873ad56a45dcdf726
duc m <25023492@vnu.edu.vn>
2026-05-16 23:36:02 +0700

    add ClientApp

--- AUCTION_SYSTEM/auction/src/main/java/auction_system/client/ClientApp.java
+++ AUCTION_SYSTEM/auction/src/main/java/auction_system/client/ClientApp.java
@@ -1,10 +1,32 @@
 package auction_system.client;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Lớp chính để khởi chạy ứng dụng Client.
  */
-public class ClientApp {
-    private ClientApp() {
-        // Lớp tiện ích không nên có constructor public
+
+public class ClientApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/client/fxml/dashboard.fxml")
+        );
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 960, 600);
+        primaryStage.setTitle("Hệ Thống Đấu Giá - Client");
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(960);
+        primaryStage.setMinHeight(600);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
